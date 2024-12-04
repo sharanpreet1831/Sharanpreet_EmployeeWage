a88d34ed8bc271fc85c84424c2109edb671ad91c
sharanpreet singh <sharanpreet1831.be21@chitkara.edu.in>
2024-12-04 14:48:01 +0530

    Sharanpreet Add : Done uc4

--- Usecase.js
+++ Usecase.js
@@ -3,12 +3,12 @@ function aborpre() {
     const  treshold = 0.5  
     if (randnumber < treshold){
       
-        console.log("absent")
+        
         return 0 ;
     }
     else {
 
-        console.log("present");
+        
         return 1 ;
 
     }
@@ -17,6 +17,7 @@ function aborpre() {
 const partTime_work  = 4 
 const FullTime_work  = 8 
 let totalworkinghours = 0;
+let Totalwages =0 ;
 
 
 function tottalhours(){
@@ -25,47 +26,54 @@ function tottalhours(){
 }
 
 
+
 function CalculateWages() {
     let worktype = Math.floor(Math.random()*3);
    
     const WagesPerHour = 20 
-    let Totalwages ;
+    
     const absentorpresent = aborpre();
     switch(absentorpresent){
         case 1 :
             switch (worktype){
                 case 0 :
-                    console.log("no work today ")
-                    Totalwages = 0 ;
-                    console.log(Totalwages);
-                    console.log(`No hours `)
+                   
+                    Totalwages += 0 ;
+                    
+                    
                     totalworkinghours +=0;
 
                     break ;
             
                 case 1 :
-                    Totalwages = partTime_work * WagesPerHour;
-                    console.log(`Total wages ${Totalwages} `)
-                    console.log(`Part time work ${partTime_work}`)
+                    Totalwages += partTime_work * WagesPerHour;
+                   
                     totalworkinghours += 4;
                   
                     break ;
                     
                 case 2 :
-                    Totalwages = FullTime_work * WagesPerHour;
-                    console.log(`Total wages ${Totalwages} `)
-                    console.log(`Fully time work ${FullTime_work}`)
+                    Totalwages += FullTime_work * WagesPerHour;
+                    
                     totalworkinghours += 8;
                     break ;
             }
             
         break 
         case 0 :
-            console.log("absent .. ");
+           Totalwages += 0;
 
     }
 
 }
 
-CalculateWages()
+
+
+
+for (var  i =0 ; i< 20  ; i++){
+    CalculateWages()
+}
 tottalhours()
+console.log(`Total wages ${Totalwages}`);
+
+
